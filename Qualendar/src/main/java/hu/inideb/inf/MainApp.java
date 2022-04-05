b4a1c616ead24679446ef78b25b84ec13e189862
la7ci9 <akarmiis997@gmail.com>
2022-04-05 18:03:27 +0200

    Build plugin hozzáadva

--- Qualendar/src/main/java/hu/inideb/inf/MainApp.java
+++ Qualendar/src/main/java/hu/inideb/inf/MainApp.java
@@ -1,6 +1,9 @@
 package hu.inideb.inf;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainApp extends Application{
@@ -8,6 +11,12 @@ public class MainApp extends Application{
 	@Override
     public void start(Stage stage) throws Exception {
 		System.out.println("Működöm");
+		
+		FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("/fxmls/MainGUI.fxml"));
+        Scene scene = new Scene((Parent) loader.load());
+        stage.setTitle("Main");
+        stage.setScene(scene);
+        stage.show();
 	}
 	
 	public static void main(String[] args) {
