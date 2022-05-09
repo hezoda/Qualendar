d9941b283e50eb88643a6a756f258b778e047257
la7ci9 <akarmiis997@gmail.com>
2022-05-09 16:31:37 +0200

    JPAEmployeeDAO osztály hozzáadva

---
+++ Qualendar/src/main/java/inf/unideb/hu/model/JPAEmployeeDAO.java
@@ -0,0 +1,47 @@
+package inf.unideb.hu.model;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+public class JPAEmployeeDAO implements EmployeeDAO {
+
+	final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("inf.unideb.hu.pu");
+    final EntityManager entityManager = entityManagerFactory.createEntityManager();
+	
+	@Override
+	public void close() throws Exception {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void saveEmployee(Employee emp) {
+		entityManager.getTransaction().begin();
+        entityManager.persist(emp);
+        entityManager.getTransaction().commit();
+	}
+
+	@Override
+	public void deleteEmployee(Employee emp) {
+		entityManager.getTransaction().begin();
+        entityManager.remove(emp);
+        entityManager.getTransaction().commit();
+		
+	}
+
+	@Override
+	public void updateEmployee(Employee emp) {
+		saveEmployee(emp);
+		
+	}
+
+	@Override
+	public List<Employee> getEmployee(Employee emp) {
+		TypedQuery<Employee> query = entityManager.createQuery("SELECT emp FROM Employee emp", Employee.class); //vagy valami ilyesmi kell
+        List<Employee> employee = query.getResultList();
+        return employee;
+	}
+
+}
--- Qualendar/src/main/java/module-info.java
+++ Qualendar/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module inf.unideb.hu {
     requires javafx.fxml;
 	requires com.h2database;
 	requires java.sql;
+	requires java.persistence;
 
     opens inf.unideb.hu.controller to javafx.fxml;
     opens inf.unideb.hu to javafx.fxml;
